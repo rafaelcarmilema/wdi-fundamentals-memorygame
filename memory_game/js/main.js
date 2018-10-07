7e916a62384c7492c3867bde83f10591c3632c69
Rafael Carmilema <rafael_carmilema@glic.com>
2018-10-07 16:19:50 -0400

    added function to main.js for card check

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,17 +3,23 @@ console.log("Up and running!");
 var cards = ["queen", "queen", "king","king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne)
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo)
-
-console.log(cardOne);
-console.log(cardTwo);
+function checkForMatch(){
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match!");
+    } else {
+        console.log("Sorry, try again.");
+    }  
+}
+function flipCard (cardId){
+    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId])
+    checkForMatch();
+}
+flipCard(3);
+flipCard(2);
 
 if(cardsInPlay.length === 2){
-    if(cardsInPlay[0]===cardsInPlay[1]){
+    if(cardsInPlay[0] === cardsInPlay[1]){
         alert("You found a match");
     }
     else{
