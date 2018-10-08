b43a4dd1d79424f519df059dd2751f9b47d6df33
Rafael Carmilema <rafael_carmilema@glic.com>
2018-10-07 20:30:13 -0400

    Completed Memory Game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,29 +1,57 @@
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "king","king"];
+var cards = [
+    {
+    rank: "Queen",
+    suit: "Hearts",
+    cardImage: "images/queen-of-hearts.png"
+    },
+    {
+    rank: "Queen",
+    suit: "Diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+    rank: "King",
+    suit: "Hearts",
+    cardImage: "images/king-of-hearts.png"
+    },
+    {
+    rank: "King",
+    suit: "Diamonds",
+    cardImage: "images/king-of-diamonds.png"
+    }
+];
 var cardsInPlay = [];
 
-function checkForMatch(){
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-        console.log("You found a match!");
-    } else {
-        console.log("Sorry, try again.");
-    }  
-}
-function flipCard (cardId){
-    console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push(cards[cardId])
-    checkForMatch();
-}
-flipCard(3);
-flipCard(2);
+function createBoard(){
+    for (var i = 0; i < cards.length; i++) {
+        var cardElement = document.createElement("img");
+        cardElement.setAttribute('src','images/back.png');
+        cardElement.setAttribute('data-id',i);
+        cardElement.addEventListener('click',flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
 
-if(cardsInPlay.length === 2){
-    if(cardsInPlay[0] === cardsInPlay[1]){
-        alert("You found a match");
-    }
-    else{
-        alert("Sorry, try again");
-    }
         
+    }
 }
+
+function checkForMatch(card,cardId){
+    //console.log(document.getElementById(cardId));
+    card.setAttribute('src',cards[cardId].cardImage)
+    if(cardsInPlay.length>=2){
+        if (cardsInPlay[0] === cardsInPlay[1]) {
+            alert("You found a match!");
+        } else {
+            alert("Sorry, try again.");
+        }  
+    }
+}
+function flipCard (){
+    var cardId = this.getAttribute('data-id');
+    var card = this;
+    cardsInPlay.push(cards[cardId].rank);
+    //console.log(this);
+    checkForMatch(card,cardId);
+}
+createBoard();
